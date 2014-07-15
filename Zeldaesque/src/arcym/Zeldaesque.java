5b60bff90c50574f04866779532c795cef375e80
arcym <andrewmcp333@gmail.com>
2014-07-15 16:19:13 -0400

    Converted from StateBasedGame to BasicGame.

--- Zeldaesque/src/arcym/Zeldaesque.java
+++ Zeldaesque/src/arcym/Zeldaesque.java
@@ -4,13 +4,13 @@ import org.lwjgl.*;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
-public class Zeldaesque extends StateBasedGame
+public class Zeldaesque extends BasicGame
 {
 	public static AppGameContainer container;
 	
 	public Zeldaesque()
 	{
-		super("Legend of Zelder");
+		super("Zeldaesque");
 	}
 	
 	public static void main(String[] args)
@@ -27,8 +27,18 @@ public class Zeldaesque extends StateBasedGame
 		}
 	}
 	
-	public void initStatesList(GameContainer arg0) throws SlickException
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException
 	{
 		//?!
+	}
+	
+	public void init(GameContainer arg0) throws SlickException
+	{
+		//?!
+	}
+	
+	public void update(GameContainer arg0, int arg1) throws SlickException
+	{
+		//?!	
 	}
 }
