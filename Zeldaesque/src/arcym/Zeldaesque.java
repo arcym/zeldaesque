dde3c91291a0adcce9556da7db8f03911ca7effd
arcym <andrewmcp333@gmail.com>
2014-07-15 16:48:57 -0400

    Loaded the data to render the room.

--- Zeldaesque/src/arcym/Zeldaesque.java
+++ Zeldaesque/src/arcym/Zeldaesque.java
@@ -2,43 +2,52 @@ package arcym;
 
 import org.lwjgl.*;
 import org.newdawn.slick.*;
-import org.newdawn.slick.state.*;
+import org.newdawn.slick.tiled.*;
 
 public class Zeldaesque extends BasicGame
 {
-	public static AppGameContainer container;
+	private TiledMap room;
 	
 	public Zeldaesque()
 	{
 		super("Zeldaesque");
 	}
+		
+	
+	public void init(GameContainer container) throws SlickException
+	{
+		try
+		{
+			room = new TiledMap("lvl/room.tmx");
+		}
+		catch (SlickException error)
+		{
+			System.out.println(error.getMessage());
+		}
+	}
+	
+	public void update(GameContainer container, int delta) throws SlickException
+	{
+	}
+	
+	public void render(GameContainer container, Graphics g) throws SlickException
+	{
+		room.render(0, 0);
+	}
+	
+	public static AppGameContainer container;
 	
 	public static void main(String[] args)
 	{
 		try
 		{
 			container = new AppGameContainer(new Zeldaesque());
-			container.setDisplayMode(640, 480, false);
+			container.setDisplayMode(11*64, 7*64, false);
 			container.start();
 		}
 		catch(Exception error)
 		{
 			System.out.println(error.getMessage());
 		}
-	}
-	
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException
-	{
-		//?!
-	}
-	
-	public void init(GameContainer arg0) throws SlickException
-	{
-		//?!
-	}
-	
-	public void update(GameContainer arg0, int arg1) throws SlickException
-	{
-		//?!	
 	}
 }
